0ac5bba0ddc7839ad9b88b7e9e1cd63982b01a7f
Scott Mebberson <scott@scottmebberson.com>
2016-03-28 10:52:23 +1030

    Now returns which objects should be removed.

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 
-var Promise = require('bluebird');
+var Promise = require('bluebird'),
+    assert = require('assert');
+
+/**
+ * The default comparator function. Simple strict equality all the way.
+ * @param  {Any} objOne The first object to compare.
+ * @param  {Any} objTwo Compare the first object to this object.
+ * @return {Boolean}    Return `true` if the object is the same, otherwise return `false`.
+ */
+function comparator (objOne, objTwo) {
+
+    // Compare an object to an object.
+    if (typeof objOne === 'object') {
+
+        try {
+            assert.deepStrictEqual(objOne, objTwo);
+        } catch (e) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    // Compare anything that is not (typeof objOne) === 'object' using the simple strict equals.
+    return objOne === objTwo;
+
+};
 
 /**
  * Data synchronisation module for Node.js.
  *
  * @param  {Array} source       Source array.
  * @param  {Array} update       An updated version of the source array.
- * @param  {Function} callback  A callback to execute with the results.
- * @return {Object}             An object which expresses which items to remove, which items to create and which items are unchanged.
+ * @param  {Function} callback  An optional callback to execute with the results.
+ * @param  {Object} opts        An object of options.
+ * @return {Promise}            A promise that will be resolved or rejected with the result (unless callback was provided).
  */
 
-module.exports = function arraySync (source, update, callback) {
+module.exports = function arraySync (source, update, opts, callback) {
 
     if (!source) {
         throw Error('You must provide a source Array for arraySync to inspect.');
@@ -20,13 +48,45 @@ module.exports = function arraySync (source, update, callback) {
         throw Error('You must provide an update Array for arraySync to inspect.');
     }
 
+    // Support four signatures:
+    //  1. (source, update, callback)
+    //  2. (source, update, opts, callback)
+    //  3, (source, update, opts)
+    //  4. (source, update)
+    if (typeof opts === 'function') {
+        callback = opts;
+        opts = {};
+    }
+
+    // Default `opts` to an Object.
+    opts = opts || {};
+
+    // Default `opts.comparator` function.
+    opts.comparator = opts.comparator || comparator;
+
+    // Return a promise (which will execute the callback if provided).
     return new Promise(function (resolve, reject) {
 
-        return resolve({
+        // Default return object.
+        var r = {
             remove: [],
             unchanged: [],
             create: []
+        };
+
+        // Find the missing values.
+        r.remove = source.filter(function (sourceValue) {
+
+            return update.find(function (element, index, array) {
+
+                return comparator(sourceValue, element) === true;
+
+            }) === undefined;
+
         });
+
+        // Resolve the result.
+        return resolve(r);
 
     }).asCallback(callback);
 
--- test/test.js
+++ test/test.js
@@ -24,6 +24,21 @@ describe('arraySync', function () {
 
     });
 
+    it('can accept an opts Object', function (done) {
+
+        arraySync([], [], {}, function (err, results) {
+
+            expect(err).to.not.exist;
+            expect(results).to.have.property('remove');
+            expect(results).to.have.property('unchanged');
+            expect(results).to.have.property('create');
+
+            return done(err);
+
+        });
+
+    });
+
     describe('via promise', function () {
 
         it('will resolve to an object with the keys remove, unchanged, create', function (done) {
@@ -59,6 +74,65 @@ describe('arraySync', function () {
                 expect(results).to.have.property('create');
 
                 return done(err);
+
+            });
+
+        });
+
+    });
+
+    describe('will', function () {
+
+        describe('determine which records should be removed', function () {
+
+            it('when working with strings', function (done) {
+
+                arraySync(['one', 'two', 'three', 'four'], ['one', 'three', 'four']).then(function (result) {
+
+                    expect(result).to.exist;
+                    expect(result).to.have.property('unchanged');
+                    expect(result).to.have.property('create');
+
+                    expect(result).to.have.property('remove');
+                    expect(result.remove).to.have.length(1);
+                    expect(result.remove[0]).to.equal('two');
+
+                    return done();
+
+                }, function (err) {
+
+                    return done(err);
+
+                });
+
+            });
+
+            it('when working with objects', function (done) {
+
+                arraySync([
+                    { type: 'node', label: 'one' },
+                    { type: 'node', label: 'two' },
+                    { type: 'node', label: 'three' }
+                ], [
+                    { type: 'node', label: 'one' },
+                    { type: 'node', label: 'three' }
+                ]).then(function (result) {
+
+                    expect(result).to.exist;
+                    expect(result).to.have.property('unchanged');
+                    expect(result).to.have.property('create');
+
+                    expect(result).to.have.property('remove');
+                    expect(result.remove).to.have.length(1);
+                    expect(result.remove[0]).to.eql({ type: 'node', label: 'two' });
+
+                    return done();
+
+                }, function (err) {
+
+                    return done(err);
+
+                });
 
             });
 
